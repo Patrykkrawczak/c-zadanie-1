fb5f7b33128603546a361440e8ee4947449ebb8a
Patryk Krawczak <patryk.krawczak@lingarogroup.com>
2020-03-27 16:06:51 +0100

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.m_box = new System.Windows.Forms.TextBox();
+            this.OK = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.x1_box = new System.Windows.Forms.TextBox();
+            this.z_box = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.x2_box = new System.Windows.Forms.TextBox();
+            this.wynik_box = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // m_box
+            // 
+            this.m_box.Location = new System.Drawing.Point(46, 155);
+            this.m_box.Multiline = true;
+            this.m_box.Name = "m_box";
+            this.m_box.Size = new System.Drawing.Size(89, 28);
+            this.m_box.TabIndex = 0;
+            this.m_box.Text = "0";
+            this.m_box.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // OK
+            // 
+            this.OK.Location = new System.Drawing.Point(46, 333);
+            this.OK.Name = "OK";
+            this.OK.Size = new System.Drawing.Size(151, 69);
+            this.OK.TabIndex = 1;
+            this.OK.Text = "OK";
+            this.OK.UseVisualStyleBackColor = true;
+            this.OK.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(353, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Patryk Krawczak - Metody numeryczne zadanie 1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // x1_box
+            // 
+            this.x1_box.Location = new System.Drawing.Point(284, 155);
+            this.x1_box.Multiline = true;
+            this.x1_box.Name = "x1_box";
+            this.x1_box.Size = new System.Drawing.Size(89, 28);
+            this.x1_box.TabIndex = 3;
+            this.x1_box.Text = "0";
+            this.x1_box.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // z_box
+            // 
+            this.z_box.Location = new System.Drawing.Point(46, 263);
+            this.z_box.Multiline = true;
+            this.z_box.Name = "z_box";
+            this.z_box.Size = new System.Drawing.Size(89, 28);
+            this.z_box.TabIndex = 4;
+            this.z_box.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Zmienne: m";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 240);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Zmienne: z";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(281, 132);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "wartość x1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(281, 240);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "wartość x2";
+            // 
+            // x2_box
+            // 
+            this.x2_box.Location = new System.Drawing.Point(284, 263);
+            this.x2_box.Multiline = true;
+            this.x2_box.Name = "x2_box";
+            this.x2_box.Size = new System.Drawing.Size(89, 28);
+            this.x2_box.TabIndex = 8;
+            this.x2_box.Text = "100";
+            // 
+            // wynik_box
+            // 
+            this.wynik_box.Location = new System.Drawing.Point(482, 155);
+            this.wynik_box.Multiline = true;
+            this.wynik_box.Name = "wynik_box";
+            this.wynik_box.Size = new System.Drawing.Size(501, 136);
+            this.wynik_box.TabIndex = 10;
+            this.wynik_box.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(478, 132);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "wynik";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1018, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.wynik_box);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.x2_box);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.z_box);
+            this.Controls.Add(this.x1_box);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.OK);
+            this.Controls.Add(this.m_box);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox m_box;
+        private System.Windows.Forms.Button OK;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox x1_box;
+        private System.Windows.Forms.TextBox z_box;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox x2_box;
+        private System.Windows.Forms.TextBox wynik_box;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+
+
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float true_wynik = 1000000 / 3;
+            Console.Out.WriteLine(m_box.Text);
+            int m = int.Parse(m_box.Text);
+            int z = int.Parse(z_box.Text);
+            Random rnd = new Random();
+            int d = 10;
+            int l = 100000;
+            float f = 0.0F;
+            for (int j = 0; j < m; j++) {
+                f = 0.0F;
+                int licz = 0;
+                float old_i = 0;
+                float n = rnd.Next(d, l);
+                for (float i = 0.0F; i <= 100.0; i = i + 100F / n)
+                {
+
+                    licz += 1;
+                    f += (i * i) * (i - old_i);
+
+                    old_i = i;
+                };
+                if (333333.333 / f > z/100.0) {
+                    string wynik = "metoda kwadratów parametry to n=";
+                    wynik += n.ToString();
+                    wynik += "  wynik to =";
+                    wynik += f.ToString();
+                    wynik += "\t\t\t";
+                    wynik_box.Text += wynik; 
+                }
+                f = 0.0F;
+
+                n = rnd.Next(d, l);
+                old_i = 0;
+                for (float i = 0.0F; i <= 100.0; i = i + 100F / n)
+                {
+
+                    licz += 1;
+                    f += (((i * i) + (old_i *old_i))/2) * (i - old_i);
+
+                    old_i = i;
+                };
+                if (333333.333 / f > z / 100.0)
+                {
+                    string wynik = "metoda trapezów parametry to n=";
+                    wynik += n.ToString();
+                    wynik += "  wynik to =";
+                    wynik += f.ToString();
+                    wynik += "\t\t";
+                    wynik_box.Text += wynik;
+                }
+            }; 
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
